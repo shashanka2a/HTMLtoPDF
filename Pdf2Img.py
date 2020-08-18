6dbc6c62a95df57489289bd8e2afd140d9ddbdf4
Shashank Jagannatham <37143174+shashanka2a@users.noreply.github.com>
2020-08-18 12:44:16 +0530

    Create Pdf2Img.py

---
+++ Pdf2Img.py
@@ -0,0 +1,11 @@
+import fitz
+import os
+
+pdfs_path = 'path_to_pdfs'
+
+for f in os.listdir(pdfs_path):
+  doc = fitz.open(pdfs_path+f)
+  page = doc.loadPage(0)  
+  pix = page.getPixmap()
+  output = imgs_path+"{}.png".format(f)
+  pix.writePNG(output)
